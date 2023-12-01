3a88b47d39222f28eb5647b6aa663eade3651ef8
Blue Bird <bluebird089@proton.me>
2023-12-02 03:18:53 +0600

    added popular content with responsive design

--- src/pages/Home/components/PopularContest.jsx
+++ src/pages/Home/components/PopularContest.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import PopularSingleContest from "./PopularSingleContest";
+import { HiMiniArrowTrendingDown } from "react-icons/hi2";
 
 const PopularContest = () => {
     const axiosPublic = useAxiosPublic();
@@ -13,16 +15,26 @@ const PopularContest = () => {
         },
     });
 
-    console.log(contests);
-
     if (isPending) {
         return <span className="loading loading-infinity loading-lg"></span>;
     }
 
     return (
         <div className="bg-white">
-            <div className="container mx-auto">
-                <div className="flex justify-center flex-wrap gap-10">
+            <div className="container mx-auto py-5 md:py-10">
+                <div className="flex items-end justify-center gap-5 pb-7">
+                    <div className="text-center space-y-2 py-3">
+                        <h3 className="text-lg md:text-3xl font-bold">Our Popular Contests</h3>
+                        <p className="text-xs md:text-base">
+                            Unleash Your Creativity: Explore, Join, Triumph in
+                            the
+                            Most Popular Contests on ContestHub's Dynamic
+                            Platform
+                        </p>
+                    </div>
+                    <HiMiniArrowTrendingDown className="hidden md:inline-block text-7xl text-gray-500" />
+                </div>
+                <div className="flex justify-center flex-wrap gap-5 md:gap-10">
                     {contests.map((contest) => (
                         <PopularSingleContest
                             key={contest.key}
--- src/pages/Home/components/PopularSingleContest.jsx
+++ src/pages/Home/components/PopularSingleContest.jsx
@@ -1,20 +1,39 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import { TbUsersGroup } from "react-icons/tb";
 
 const PopularSingleContest = ({ contest }) => {
     const { contestImage, contestName, participantsCount, shortDescription } =
         contest;
 
+    const [showFullContent, setShowFullContent] = useState(false);
+
+    const visibleContent = showFullContent
+        ? shortDescription
+        : shortDescription.slice(0, 50);
+
+    const handleReadMore = () => {
+        setShowFullContent(!showFullContent);
+    };
+
     return (
-        <div className="w-1/4">
-            <div className="bg-white border-2 border-gray-400 rounded-lg shadow-lg custom-shadow p-3 flex flex-col items-start gap-2 h-full">
+        <div className="w-2/5 lg:w-1/4">
+            <div className="bg-white border-2 border-gray-400 rounded-xl shadow-lg custom-shadow p-1 md:p-3 items-start gap-1 md:gap-2 flex flex-col">
                 <img className="rounded-lg" src={contestImage} alt="" />
-                <h2 className="text-2xl font-bold flex-1">{contestName}</h2>
-                <h3 className="flex items-center gap-1">
+                <h2 className="text-sm md:text-2xl font-bold">{contestName}</h2>
+                <h3 className="flex items-center text-xs md:text-base gap-1">
                     <TbUsersGroup /> Attempted Count: {participantsCount}
                 </h3>
-                <p className="h-full flex-1">{shortDescription}</p>
-                <button className="bg-[#E6B8A4] btn btn-sm rounded-full border-none hover:bg-[#B38F7F] text-white font-bold">
+                <p className="h-full text-xs sm:text-sm md:text-base">
+                    {visibleContent}{" "}
+                    <button
+                        className="text-[#9BD3D0] hover:underline"
+                        onClick={handleReadMore}
+                    >
+                        {showFullContent ? "Show Less" : "...Read More"}
+                    </button>{" "}
+                </p>
+                <button className="bg-[#E6B8A4] text-xs sm:text-base btn btn-sm rounded-full border-none hover:bg-[#B38F7F] text-white font-bold">
                     Details
                 </button>
             </div>
