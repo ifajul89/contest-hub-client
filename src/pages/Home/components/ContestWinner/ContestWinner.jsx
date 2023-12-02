fdbf008ef34098ae83e167f58a8dbb5a6947d7e1
Blue Bird <bluebird089@proton.me>
2023-12-02 18:14:05 +0600

    added basic structure for contest winner sectoin

--- src/pages/Home/components/ContestWinner/ContestWinner.jsx
+++ src/pages/Home/components/ContestWinner/ContestWinner.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
-import Image from "../../../../assets/colorful-pattern-young-people_23-2148218806.jpg";
-import "./ContestWinner.css"
+import "./ContestWinner.css";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 
 const carousel = (slider) => {
-    const z = 300;
+    const z = 250;
     function rotate() {
         const deg = 360 * slider.track.details.progress;
         slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`;
@@ -29,23 +31,81 @@ export default function ContestWinner() {
             renderMode: "custom",
             mode: "free-snap",
         },
-        [carousel]
+        [
+            (slider) => {
+                let timeout;
+                let mouseOver = false;
+                function clearNextTimeout() {
+                    clearTimeout(timeout);
+                }
+                function nextTimeout() {
+                    clearTimeout(timeout);
+                    if (mouseOver) return;
+                    timeout = setTimeout(() => {
+                        slider.next();
+                    }, 4000);
+                }
+                slider.on("created", () => {
+                    slider.container.addEventListener("mouseover", () => {
+                        mouseOver = true;
+                        clearNextTimeout();
+                    });
+                    slider.container.addEventListener("mouseout", () => {
+                        mouseOver = false;
+                        nextTimeout();
+                    });
+                    nextTimeout();
+                });
+                slider.on("dragStarted", clearNextTimeout);
+                slider.on("animationEnded", nextTimeout);
+                slider.on("updated", nextTimeout);
+            },
+            carousel,
+        ]
     );
 
+    const axiosPublic = useAxiosPublic();
+
+    const { data: winners, isPending } = useQuery({
+        queryKey: ["winners"],
+        queryFn: async () => {
+            const res = await axiosPublic.get("/top-winner");
+            return res.data;
+        },
+    });
+
+    if (isPending) {
+        return <span className="loading loading-infinity loading-lg"></span>;
+    }
+
+    console.log(winners);
+
     return (
-        <div className="wrapper">
-            <div className="scene">
-                <div className="carousel keen-slider" ref={sliderRef}>
-                    <div className="carousel__cell number-slide1">
-                        <img src={Image} alt="" />
-                    </div>
-                    <div className="carousel__cell number-slide2">
-                        <img src={Image} alt="" />
-                    </div>
-                    <div className="carousel__cell number-slide3">
-                        <img src={Image} alt="" />
+        <div className="flex justify-center gap-32 items-center mt-10 mb-20">
+            <div className="wrapper">
+                <div className="scene">
+                    <div className="carousel keen-slider" ref={sliderRef}>
+                        {winners.map((winner, index) => (
+                            <div
+                                key={winner._id}
+                                className={`carousel__cell number-slide${
+                                    index + 1
+                                } w-full`}
+                            >
+                                <img src={winner.winnerImage} alt="" />
+                            </div>
+                        ))}
                     </div>
                 </div>
+            </div>
+            <div className="w-1/3">
+                <h3>See Our Winners</h3>
+                <p>
+                    Kudos to our recent contest winners! Now, it's YOUR turn to
+                    shine. Join our contests, showcase your creativity, and be
+                    the next star in our "Contest Winners" section. Unleash your
+                    talent, inspire others, and let the celebrations begin!
+                </p>
             </div>
         </div>
     );
