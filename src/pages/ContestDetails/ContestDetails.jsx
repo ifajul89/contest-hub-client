be67d1da8536926ba22e738ae5cb1a84fc24a619
Blue Bird <bluebird089@proton.me>
2023-12-16 03:20:34 +0600

    added basic timer to contest details page

--- src/pages/ContestDetails/ContestDetails.jsx
+++ src/pages/ContestDetails/ContestDetails.jsx
@@ -1,5 +1,7 @@
 import { TbUsersGroup } from "react-icons/tb";
 import { Link, useLoaderData } from "react-router-dom";
+import moment from "moment";
+import { useEffect, useState } from "react";
 
 const ContestDetails = () => {
     const {
@@ -11,8 +13,65 @@ const ContestDetails = () => {
         winnerName,
         winnerImage,
         colorCode,
-        // Show Dynamic Deadline
+        contestDeadline,
     } = useLoaderData();
+
+    const [daysLeft, setDaysLeft] = useState("");
+    const [hoursLeft, setHoursLeft] = useState("");
+    const [minutesLeft, setMinutesLeft] = useState("");
+    const [secondsLeft, setSecondsLeft] = useState("");
+
+    const [winner, setWinner] = useState("Not Selected");
+
+    const deadLine = moment(contestDeadline);
+
+    const getTimeRemaining = () => {
+        const currentTime = moment();
+        const duration = moment.duration(deadLine.diff(currentTime));
+        return {
+            days: Math.floor(duration.asDays()),
+            hours: duration.hours(),
+            minutes: duration.minutes(),
+            seconds: duration.seconds(),
+        };
+    };
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTimeRemaining(getTimeRemaining());
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const [timeRemaining, setTimeRemaining] = useState(getTimeRemaining());
+
+    useEffect(() => {
+        if (
+            timeRemaining.days <= 0 &&
+            timeRemaining.hours <= 0 &&
+            timeRemaining.minutes <= 0 &&
+            timeRemaining.seconds <= 0
+        ) {
+            handleTimerEnd();
+        }
+    }, [timeRemaining]);
+
+    useEffect(() => {
+        setDaysLeft(timeRemaining.days);
+        setHoursLeft(timeRemaining.hours);
+        setMinutesLeft(timeRemaining.minutes);
+        setSecondsLeft(timeRemaining.seconds);
+    }, [
+        timeRemaining.days,
+        timeRemaining.hours,
+        timeRemaining.minutes,
+        timeRemaining.seconds,
+    ]);
+
+    const handleTimerEnd = () => {
+        setWinner("selected");
+    };
 
     return (
         <div
@@ -36,20 +95,48 @@ const ContestDetails = () => {
                         <TbUsersGroup /> Attempted Count: {participantsCount}
                     </h3>
                     <h4 className="text-sm md:text-base">{shortDescription}</h4>
-                    <div className="flex items-center gap-3">
-                        <h3 className="md:text-xl font-bold">Winner Info:</h3>
-                        <span className="flex gap-2 items-center p-2 border-2 border-gray-900 rounded-full">
-                            <img
-                                className="rounded-full w-10"
-                                src={winnerImage}
-                                alt=""
-                            />
-                            <h3 className="md:text-xl font-bold">
-                                {winnerName}
+                    {winner === "Not Selected" ? (
+                        <>
+                            <h3 className="text-2xl font-extrabold">
+                                Times Left :
                             </h3>
-                        </span>
-                    </div>
-                    <p>{/* Dynamic Timer */}</p>
+                            <div className="flex text-xl font-bold items-center gap-5">
+                                <div className="text-center">
+                                    <p>{daysLeft}</p>
+                                    <p>Day</p>
+                                </div>
+                                <div className="text-center">
+                                    <p>{hoursLeft}</p>
+                                    <p>Hour</p>
+                                </div>
+                                <div className="text-center">
+                                    <p>{minutesLeft}</p>
+                                    <p>Minute</p>
+                                </div>
+                                <div className="text-center">
+                                    <p>{secondsLeft}</p>
+                                    <p>Second</p>
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <div className="flex items-center gap-3">
+                            <h3 className="md:text-xl font-bold">
+                                Winner Info:
+                            </h3>
+                            <span className="flex gap-2 items-center p-2 border-2 border-gray-900 rounded-full">
+                                <img
+                                    className="rounded-full w-10"
+                                    src={winnerImage}
+                                    alt=""
+                                />
+                                <h3 className="md:text-xl font-bold">
+                                    {winnerName}
+                                </h3>
+                            </span>
+                        </div>
+                    )}
+
                     <Link
                         to={`/purchase/${_id}`}
                         className="btn w-full rounded-full text-white border-none bg-gray-900 hover:bg-black"
