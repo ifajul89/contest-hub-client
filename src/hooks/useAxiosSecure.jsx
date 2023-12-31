be9fbab200a424ce21b94774975e70fd0c7e2762
Blue Bird <bluebird089@proton.me>
2023-12-31 23:24:33 +0600

    added vercel api link for testing

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://b8a12-server-side-bluebird089.vercel.app",
 });
 
 const useAxiosSecure = () => {
